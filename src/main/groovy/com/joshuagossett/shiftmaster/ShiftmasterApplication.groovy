bf147f71aa702980fbe8ed71e6a2f60b4474bcb0
Joshua Gossett <joshua.gossett@gmail.com>
2021-07-16 14:34:52 -0500

    Initial source code.

---
+++ src/main/groovy/com/joshuagossett/shiftmaster/ShiftmasterApplication.groovy
@@ -0,0 +1,13 @@
+package com.joshuagossett.shiftmaster
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ShiftmasterApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ShiftmasterApplication, args)
+	}
+
+}
