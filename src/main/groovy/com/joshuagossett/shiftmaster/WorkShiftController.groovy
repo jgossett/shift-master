01660b372dbd388c3cf96dde2697c694ca3b9659
Joshua Gossett <joshua.gossett@gmail.com>
2021-07-21 13:46:34 -0500

    created index controller method.

--- src/main/groovy/com/joshuagossett/shiftmaster/WorkShiftController.groovy
+++ src/main/groovy/com/joshuagossett/shiftmaster/WorkShiftController.groovy
@@ -1,11 +1,15 @@
 package com.joshuagossett.shiftmaster
 
 import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.data.domain.Example
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.function.EntityResponse
+
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping(path = "/WorkShift", produces = [MediaType.APPLICATION_JSON_VALUE])
@@ -14,8 +18,17 @@ class WorkShiftController {
     WorkShiftService workShiftService
 
     @GetMapping
-    def index() {
-       workShiftService.findAll()
+    def index(
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            @RequestParam(required = false) LocalDateTime startTime,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            @RequestParam(required = false) LocalDateTime endTime
+    ) {
+        workShiftService.findAllByBetweenStartTimeAndEndTime(startTime, endTime)
+//        workShiftService.findAllByBetweenStartTimeAndEndTime()
+
+//        workShiftService.findAll(Example.of(probe))
+//        workShiftService.findAll(Sort.by('startTime'))
     }
 
 }
--- src/main/groovy/com/joshuagossett/shiftmaster/WorkShiftService.groovy
+++ src/main/groovy/com/joshuagossett/shiftmaster/WorkShiftService.groovy
@@ -1,10 +1,27 @@
 package com.joshuagossett.shiftmaster
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
+
+import java.time.LocalDateTime
 
 @Repository
 @Transactional(readOnly = true)
 interface WorkShiftService extends JpaRepository<WorkShift, Long> {
+
+    @Query("""
+        SELECT ws 
+        FROM WorkShift ws
+        WHERE (:startTime IS NULL or :startTime <= ws.startTime)
+            AND (:endTime IS NULL or ws.endTime <= :endTime)
+        ORDER BY ws.startTime DESC
+     """)
+    List<WorkShift> findAllByBetweenStartTimeAndEndTime(
+            @Param("startTime") LocalDateTime startTime,
+            @Param("endTime") LocalDateTime endTime
+    )
+
 }
